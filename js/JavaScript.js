f72135394d30cd2b2abe4b319773be1b0f109ec9
tobiaspossetto <tobigpossetto@gmail.com>
2021-03-11 22:32:29 -0300

    first commit

---
+++ js/JavaScript.js
@@ -0,0 +1,22 @@
+(function() {
+
+
+    //variables
+    let lista = document.getElementById('notas-ul');
+    let tituloInput = document.getElementById('titulo');
+    let descripcionInput = document.getElementById('descripcion');
+    let btnCrear = document.getElementById('CreadorNotas-crear');
+
+
+
+    mensaje = function() {
+
+        alert('sdsd')
+    }
+
+
+
+    btnCrear.addEventListener('click', mensaje());
+
+
+}());
